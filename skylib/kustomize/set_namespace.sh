6ffcfb3028013e089061e8a980afb42f457eb1b2
apesternikov <apesternikov@gmail.com>
2023-02-22 16:48:26 -0800

    use bash runfiles library to find location of the kustomize binary (#124)

--- skylib/kustomize/set_namespace.sh
+++ skylib/kustomize/set_namespace.sh
@@ -11,6 +11,17 @@
 
 set +x
 
+# --- begin runfiles.bash initialization v3 ---
+# Copy-pasted from the Bazel Bash runfiles library v3.
+set -uo pipefail; set +e; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v3 ---
+
 if [ "$1" == "" ]; then
     echo usage:
     echo $0 'namespace <in.yaml >out.yaml'
@@ -24,4 +35,5 @@ namespace: $1
 resources:
 - in.yaml
 EOF
-exec external/kustomize_bin/kustomize build ${dir}
+KUSTOMIZE_BIN="$(rlocation kustomize_bin/kustomize)"
+exec $KUSTOMIZE_BIN build ${dir}
